5fe51f3dd55caf8eaf6307e4ba95a63d1caa40ae
cghael <a.p.gorobets@gmail.com>
2020-02-13 13:19:07 +0300

    Everything done to start our super-mega project!

---
+++ cmake-build-debug/CMakeFiles/fdf.dir/DependInfo.cmake
@@ -0,0 +1,91 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/cghael/fdf/libft/src/ft_atoi.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_atoi.c.o"
+  "/Users/cghael/fdf/libft/src/ft_bzero.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_bzero.c.o"
+  "/Users/cghael/fdf/libft/src/ft_delnode.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_delnode.c.o"
+  "/Users/cghael/fdf/libft/src/ft_get_next_line.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_get_next_line.c.o"
+  "/Users/cghael/fdf/libft/src/ft_isalnum.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_isalnum.c.o"
+  "/Users/cghael/fdf/libft/src/ft_isalpha.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_isalpha.c.o"
+  "/Users/cghael/fdf/libft/src/ft_isascii.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_isascii.c.o"
+  "/Users/cghael/fdf/libft/src/ft_isdigit.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_isdigit.c.o"
+  "/Users/cghael/fdf/libft/src/ft_islower.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_islower.c.o"
+  "/Users/cghael/fdf/libft/src/ft_isprint.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_isprint.c.o"
+  "/Users/cghael/fdf/libft/src/ft_isspace.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_isspace.c.o"
+  "/Users/cghael/fdf/libft/src/ft_isupper.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_isupper.c.o"
+  "/Users/cghael/fdf/libft/src/ft_itoa.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_itoa.c.o"
+  "/Users/cghael/fdf/libft/src/ft_lstadd.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_lstadd.c.o"
+  "/Users/cghael/fdf/libft/src/ft_lstdel.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_lstdel.c.o"
+  "/Users/cghael/fdf/libft/src/ft_lstdelone.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_lstdelone.c.o"
+  "/Users/cghael/fdf/libft/src/ft_lstiter.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_lstiter.c.o"
+  "/Users/cghael/fdf/libft/src/ft_lstlen.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_lstlen.c.o"
+  "/Users/cghael/fdf/libft/src/ft_lstmap.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_lstmap.c.o"
+  "/Users/cghael/fdf/libft/src/ft_lstnew.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_lstnew.c.o"
+  "/Users/cghael/fdf/libft/src/ft_memalloc.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_memalloc.c.o"
+  "/Users/cghael/fdf/libft/src/ft_memccpy.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_memccpy.c.o"
+  "/Users/cghael/fdf/libft/src/ft_memchr.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_memchr.c.o"
+  "/Users/cghael/fdf/libft/src/ft_memcmp.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_memcmp.c.o"
+  "/Users/cghael/fdf/libft/src/ft_memcpy.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_memcpy.c.o"
+  "/Users/cghael/fdf/libft/src/ft_memdel.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_memdel.c.o"
+  "/Users/cghael/fdf/libft/src/ft_memmove.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_memmove.c.o"
+  "/Users/cghael/fdf/libft/src/ft_memset.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_memset.c.o"
+  "/Users/cghael/fdf/libft/src/ft_putchar.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_putchar.c.o"
+  "/Users/cghael/fdf/libft/src/ft_putchar_fd.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_putchar_fd.c.o"
+  "/Users/cghael/fdf/libft/src/ft_putendl.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_putendl.c.o"
+  "/Users/cghael/fdf/libft/src/ft_putendl_fd.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_putendl_fd.c.o"
+  "/Users/cghael/fdf/libft/src/ft_putnbr.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_putnbr.c.o"
+  "/Users/cghael/fdf/libft/src/ft_putnbr_fd.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_putnbr_fd.c.o"
+  "/Users/cghael/fdf/libft/src/ft_putstr.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_putstr.c.o"
+  "/Users/cghael/fdf/libft/src/ft_putstr_fd.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_putstr_fd.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strcat.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strcat.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strchr.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strchr.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strclr.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strclr.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strcmp.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strcmp.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strcpy.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strcpy.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strdel.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strdel.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strdup.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strdup.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strequ.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strequ.c.o"
+  "/Users/cghael/fdf/libft/src/ft_striter.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_striter.c.o"
+  "/Users/cghael/fdf/libft/src/ft_striteri.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_striteri.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strjoin.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strjoin.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strlcat.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strlcat.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strlen.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strlen.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strmap.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strmap.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strmapi.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strmapi.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strncat.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strncat.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strncmp.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strncmp.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strncpy.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strncpy.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strnequ.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strnequ.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strnew.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strnew.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strnlen.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strnlen.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strnstr.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strnstr.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strrchr.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strrchr.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strsplit.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strsplit.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strstr.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strstr.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strsub.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strsub.c.o"
+  "/Users/cghael/fdf/libft/src/ft_strtrim.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_strtrim.c.o"
+  "/Users/cghael/fdf/libft/src/ft_tolower.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_tolower.c.o"
+  "/Users/cghael/fdf/libft/src/ft_toupper.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/src/ft_toupper.c.o"
+  "/Users/cghael/fdf/src/main.c" "/Users/cghael/fdf/cmake-build-debug/CMakeFiles/fdf.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../includes"
+  "../libft"
+  "../libft/includes"
+  "../libft/src"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
