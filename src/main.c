e1da24cc8a715370dfaa3e6e8633a8831b401d32
ksemele <ksemele@student.21-school.ru>
2020-02-17 15:49:43 +0300

    printing key_codes by press keys in window, put_pixel working))))

--- src/main.c
+++ src/main.c
@@ -13,17 +13,31 @@
 #include "fdf.h"
 #include "mlx.h"
 
+int				ft_deal_key(int key, t_mlx *param)
+{
+	if (key != 900)
+		ft_printf("\n", param);
+	ft_printf("key is: int [%1$d]", key);
+	mlx_pixel_put(param->mlx_ptr, param->win_ptr, 50, 50, 255);
+	return (0);
+}
+
 int				main(int argc, char **argv)
 {
 	void		*mlx_ptr;
 	void		*win_ptr;
+	t_mlx		*mlx_s;
 
 	ft_check_args(argc, argv);
 	ft_putstr(LEAK);
 	perror("main()");
+	mlx_s = malloc(sizeof(t_mlx));
 	ft_printf("\e[1;36moh, \e[1;31myes! \e[1;32mthis is ft_printf! \e[1;36m:3\e[m\n");
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 100, 100, "lol");
+	mlx_s->mlx_ptr = mlx_ptr;
+	win_ptr = mlx_new_window(mlx_ptr, 100, 100, "fdf");
+	mlx_s->win_ptr = win_ptr;
+	mlx_key_hook(win_ptr, ft_deal_key, mlx_s);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
