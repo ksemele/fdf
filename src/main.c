eca3ac1821df496a7a5a7ce97bb630f9c5908644
ksemele <greengrunt@gmail.com>
2020-02-18 15:20:19 +0300

    add simple exit by press "Esc"

--- src/main.c
+++ src/main.c
@@ -13,14 +13,22 @@
 #include "fdf.h"
 #include "mlx.h"
 
-int				ft_deal_key(int key, t_mlx *mlx_s)
-{
 #define CYBER 0x9B1F6A
-	if (key != 900)
-		ft_printf("\n", mlx_s);
-	ft_printf("key is: int [%1$d]", key);
+static int				ft_deal_key(int key, t_mlx *mlx_s)
+{
 	mlx_s->color = (int)CYBER;
+	mlx_string_put(mlx_s->mlx_ptr, mlx_s->win_ptr, 100, 100, mlx_s->color, "EBAT 1");
+
+	ft_printf("key is: int [%1$d]\n", key);//нажали кнопку - зажгли пиксель
 	mlx_pixel_put(mlx_s->mlx_ptr, mlx_s->win_ptr, 50, 50, mlx_s->color);
+	mlx_s->color <<= 16;
+	mlx_string_put(mlx_s->mlx_ptr, mlx_s->win_ptr, 100, 100, mlx_s->color, "EBAT 2");
+	if (key == 53)
+	{
+		mlx_destroy_window(mlx_s->mlx_ptr, mlx_s->win_ptr);
+//		ft_free();//todo
+		exit (0);
+	}
 	return (0);
 }
 
@@ -34,7 +42,7 @@ int				main(int argc, char **argv)
 	mlx_s = malloc(sizeof(t_mlx));//todo add ft_create_t_mlx() ft_bzero, etc
 	ft_printf("\e[1;36moh, \e[1;31myes! \e[1;32mthis is ft_printf! \e[1;36m:3\e[m\n");
 	mlx_s->mlx_ptr = mlx_init();
-	mlx_s->win_ptr = mlx_new_window(mlx_s->mlx_ptr, 100, 100, "fdf");
+	mlx_s->win_ptr = mlx_new_window(mlx_s->mlx_ptr, 800, 600, "fdf");
 	mlx_key_hook(mlx_s->win_ptr, ft_deal_key, mlx_s);
 	mlx_loop(mlx_s->mlx_ptr);
 	return (0);
