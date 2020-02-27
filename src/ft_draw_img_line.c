ffca8758c01227df4c8102779831a785693d528e
cghael <a.p.gorobets@gmail.com>
2020-02-27 14:43:30 +0300

    img line draw well

--- src/ft_draw_img_line.c
+++ src/ft_draw_img_line.c
@@ -16,13 +16,13 @@ static void		ft_draw_img_pixel(t_mlx *mlx, t_point *point)
 {
 	int			i;
 
-	if ((int)point->x_d <= mlx->win_x && (int)point->y_d >= 0 && \
-			(int)point->y_d <= mlx->win_y)//TODO px->y_d >(>=) 0 ?
+	if ((int)point->x_d < mlx->win_x && (int)point->y_d >= 0 && \
+			(int)point->y_d < mlx->win_y)//TODO px->y_d >(>=) 0 ?
 	{
 		i = ((int)point->x_d * mlx->bpp / 8) + ((int)point->y_d * mlx->width);
 		mlx->pixels[i] = (char)point->color;
-		mlx->pixels[i + 1] = point->color >> 8;
-		mlx->pixels[i + 2] = point->color >> 16;
+		mlx->pixels[i + 1] = (char)((unsigned)point->color >> 8u);
+		mlx->pixels[i + 2] = (char)((unsigned)point->color >> 16u);
 	}
 }
 
--- src/ft_draw_img_wireframe.c
+++ src/ft_draw_img_wireframe.c
@@ -22,19 +22,16 @@ void		ft_draw_img_wireframe(t_mlx mlx)
 	ft_draw_background(&mlx);
 	while (y < mlx.map.len_y)
 	{
-//		ft_printf("total points %d\n", mlx.map.total_points);
-//		ft_printf("x %d\tx+max %d\n", x, x + mlx.map.len_x);
-		while (x <= mlx.map.total_points && x + 1 <= mlx.map.total_points)
+		while (x + 1 <= mlx.map.total_points && x < mlx.map.total_points)
 		{
+
 			if (mlx.map.px[x].y == mlx.map.px[x + 1].y && x >= 0 )
 			{
-//				if(mlx.map.px[x + 1].color && mlx.map.px[x].color)
 					ft_draw_img_line(mlx.map.px[x],
 									 mlx.map.px[x + 1], &mlx);
 			}
-			if (mlx.map.px[x].x == mlx.map.px[x + mlx.map.len_x].x && x + mlx.map.len_x <= mlx.map.total_points)
+			if (x + mlx.map.len_x < mlx.map.total_points && mlx.map.px[x].x == mlx.map.px[x + mlx.map.len_x].x)
 			{
-//				if(mlx.map.px[x + mlx.map.len_x].color && mlx.map.px[x].color)
 					ft_draw_img_line(mlx.map.px[x], \
 						mlx.map.px[x + mlx.map.len_x], &mlx);
 			}
