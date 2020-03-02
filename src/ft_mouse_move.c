daa4601de378f7dd6ea31708233b712c37d5e9b4
ksemele <greengrunt@gmail.com>
2020-03-02 12:35:07 +0300

    Update ft_mouse_move.c

--- src/ft_mouse_move.c
+++ src/ft_mouse_move.c
@@ -24,40 +24,32 @@ int			ft_mouse_move(int x, int y, void *param)
 	mlx->mouse.y = y;
 	if (mlx->mouse.is_pressed)
 	{
-//		mlx->camera->beta += (x - mlx->mouse.previous_x) * 0.002;
-//		mlx->camera->alpha += (y - mlx->mouse.previous_y) * 0.002;
-////		draw(mlx->map, mlx);
-//ft_printf("m_ move %d\n", mlx->mouse.x);
-//ft_printf("mousex = %d | %d\n", mlx->mouse.previous_x,mlx->mouse.x);
-//ft_printf("x %d y %d\n", x, y);
-
-	if (mlx->mouse.x > mlx->mouse.previous_x)
-	{
-		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-		mlx->slide = SLIDE;
-		ft_slide_x(mlx);
-	}
-	if (mlx->mouse.x < mlx->mouse.previous_x)
-	{
-		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-		mlx->slide = -SLIDE;
-		ft_slide_x(mlx);
-	}
-	if (mlx->mouse.y > mlx->mouse.previous_y)
-	{
-		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-		mlx->slide = SLIDE;
-		ft_slide_y(mlx);
-	}
-	if (mlx->mouse.y < mlx->mouse.previous_y)
-	{
-		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-		mlx->slide = -SLIDE;
-		ft_slide_y(mlx);
-	}
-	ft_draw_img_wireframe(*mlx);
-	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_ptr, 50, 50);
-
+		if (mlx->mouse.x > mlx->mouse.previous_x)
+		{
+			mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+			mlx->slide = SLIDE;
+			ft_slide_x(mlx);
+		}
+		if (mlx->mouse.x < mlx->mouse.previous_x)
+		{
+			mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+			mlx->slide = -SLIDE;
+			ft_slide_x(mlx);
+		}
+		if (mlx->mouse.y > mlx->mouse.previous_y)
+		{
+			mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+			mlx->slide = SLIDE;
+			ft_slide_y(mlx);
+		}
+		if (mlx->mouse.y < mlx->mouse.previous_y)
+		{
+			mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+			mlx->slide = -SLIDE;
+			ft_slide_y(mlx);
+		}
+		ft_draw_img_wireframe(*mlx);
+		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_ptr, 50, 50);
 	}
 	return (0);
 }
