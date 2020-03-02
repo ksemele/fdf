9e78753505f3f816ab95a36eb6ef5e72f8cd3ef1
ksemele <greengrunt@gmail.com>
2020-03-02 12:38:23 +0300

    Update ft_mouse_move.c

--- src/ft_mouse_move.c
+++ src/ft_mouse_move.c
@@ -13,9 +13,41 @@
 #include "fdf.h"
 #define SLIDE 10
 
-int			ft_mouse_move(int x, int y, void *param)
+static void		ft_mouse_slide_x(int x, int y, t_mlx *mlx)
 {
-	t_mlx	*mlx;
+	if (mlx->mouse.x > mlx->mouse.previous_x)
+	{
+		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+		mlx->slide = SLIDE;
+		ft_slide_x(mlx);
+	}
+	if (mlx->mouse.x < mlx->mouse.previous_x)
+	{
+		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+		mlx->slide = -SLIDE;
+		ft_slide_x(mlx);
+	}
+}
+
+static void		ft_mouse_slide_y(int x, int y, t_mlx *mlx)
+{
+	if (mlx->mouse.y > mlx->mouse.previous_y)
+	{
+		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+		mlx->slide = SLIDE;
+		ft_slide_y(mlx);
+	}
+	if (mlx->mouse.y < mlx->mouse.previous_y)
+	{
+		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+		mlx->slide = -SLIDE;
+		ft_slide_y(mlx);
+	}
+}
+
+int				ft_mouse_move(int x, int y, void *param)
+{
+	t_mlx		*mlx;
 
 	mlx = (t_mlx *)param;
 	mlx->mouse.previous_x = mlx->mouse.x;
@@ -24,30 +56,8 @@ int			ft_mouse_move(int x, int y, void *param)
 	mlx->mouse.y = y;
 	if (mlx->mouse.is_pressed)
 	{
-		if (mlx->mouse.x > mlx->mouse.previous_x)
-		{
-			mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-			mlx->slide = SLIDE;
-			ft_slide_x(mlx);
-		}
-		if (mlx->mouse.x < mlx->mouse.previous_x)
-		{
-			mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-			mlx->slide = -SLIDE;
-			ft_slide_x(mlx);
-		}
-		if (mlx->mouse.y > mlx->mouse.previous_y)
-		{
-			mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-			mlx->slide = SLIDE;
-			ft_slide_y(mlx);
-		}
-		if (mlx->mouse.y < mlx->mouse.previous_y)
-		{
-			mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
-			mlx->slide = -SLIDE;
-			ft_slide_y(mlx);
-		}
+		ft_mouse_slide_x(x, y, mlx);
+		ft_mouse_slide_y(x, y, mlx);
 		ft_draw_img_wireframe(*mlx);
 		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_ptr, 50, 50);
 	}
