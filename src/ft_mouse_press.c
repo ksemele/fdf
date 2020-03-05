b9f1c09b7c059c0b4c0d4f2f938ac573d0b2718c
ksemele <greengrunt@gmail.com>
2020-03-05 13:11:48 +0300

    scroll mouse ver 0.1

--- src/ft_mouse_press.c
+++ src/ft_mouse_press.c
@@ -19,12 +19,45 @@ int			ft_mouse_press(int key, int x, int y, void *param)
 	(void)x;
 	(void)y;
 	mlx = (t_mlx *)param;
-	if (key == MOUSE_SCROLL_UP || key == MOUSE_SCROLL_DOWN)
+	if (key == MOUSE_SCROLL_UP)
 	{
-//		ft_zoom(key, mlx);//TODO ft_zoom()
-	ft_printf("scrolling\n");
+		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+		if ((mlx->scale < 50) && (mlx->scale >= 1))
+		{
+			mlx->scale += 1;
+//			mlx->scale_z = mlx->scale / 2;
+		}
+		ft_scale_points(mlx);
+	}
+	if (key == MOUSE_SCROLL_DOWN)
+	{
+		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+		if (mlx->scale >= 2)
+		{
+			mlx->scale -= 1;
+//			mlx->scale_z = mlx->scale / 2;
+		}
+		ft_scale_points(mlx);
 	}
 	else if (key == MOUSE_LEFT_BUTTON && mlx->mouse.x > 0 && mlx->mouse.y > 0)
 		mlx->mouse.is_pressed = 1;
+	if (mlx->map.iso == 1)
+		ft_img_isometric(mlx);
+	else if (mlx->map.iso == 2)
+		ft_img_isometric2(mlx);
+	else if (mlx->map.iso == 3)
+		ft_img_isometric3(mlx);
+	ft_draw_img_wireframe(*mlx);
+	ft_draw_img_line(mlx->map.center, mlx->map.px[0], mlx); //todo del
+	if (mlx->pressed == SPACE)
+	{
+		mlx_clear_window(mlx->mlx_ptr, mlx->win_ptr);
+		mlx_destroy_image(mlx->mlx_ptr, mlx->img_ptr);
+		mlx->img_ptr = mlx_new_image(mlx->mlx_ptr, mlx->win_x, mlx->win_y);
+		ft_draw_background(mlx);
+	}
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_ptr, 0, 0);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_menu_ptr, 0, 0);
+	ft_draw_img_menu_strings(mlx);
 	return (0);
 }
