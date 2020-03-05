44569c1fa12127806c50a5b1a6b30f2f9cc61033
ksemele <greengrunt@gmail.com>
2020-03-05 13:08:15 +0300

    zoom v 0.2

--- src/ft_scale_points.c
+++ src/ft_scale_points.c
@@ -25,26 +25,26 @@ void		ft_scale_points(t_mlx *mlx)
 //			mlx->map.px[i].z_w *= mlx->scale;
 //			if (round(mlx->map.px[i].z_w) == 0)
 //				mlx->map.px[i].z_w = mlx->map.px[i].z_w * mlx->scale + mlx->map.px[i].z;
-			mlx->map.px[i].z_w = mlx->map.px[i].z * mlx->scale;
-			mlx->map.px[i].y_w = mlx->map.px[i].y * mlx->scale;
-			mlx->map.px[i].x_w = mlx->map.px[i].x * mlx->scale;
+			mlx->map.px[i].z_w = (double)mlx->map.px[i].z * mlx->scale;
+			mlx->map.px[i].y_w = (double)mlx->map.px[i].y * mlx->scale;
+			mlx->map.px[i].x_w = (double)mlx->map.px[i].x * mlx->scale;
 			i++;
 		}
 	}
-	else if (mlx->scale < 0)
-	{
-		while (i < mlx->map.total_points)
-		{
-//			tmp = mlx->map.px[i].y_w;
-//			mlx->map.px[i].z_w /= mlx->scale;
-//			if (round(mlx->map.px[i].z_w) == 0)
-//				mlx->map.px[i].z_w = (mlx->map.px[i].z_w / mlx->scale) + mlx->map.px[i].z;
-			mlx->map.px[i].z_w = mlx->map.px[i].z / mlx->scale;
-			mlx->map.px[i].y_w = mlx->map.px[i].y / mlx->scale;
-			mlx->map.px[i].x_w = mlx->map.px[i].x / mlx->scale;
-//			mlx->map.px[i].z_w = mlx->map.px[i].y_w;
-			i++;
-		}
-	}
+//	else if (mlx->scale < 0)
+//	{
+//		while (i < mlx->map.total_points)
+//		{
+////			tmp = mlx->map.px[i].y_w;
+////			mlx->map.px[i].z_w /= mlx->scale;
+////			if (round(mlx->map.px[i].z_w) == 0)
+////				mlx->map.px[i].z_w = (mlx->map.px[i].z_w / mlx->scale) + mlx->map.px[i].z;
+//			mlx->map.px[i].z_w = mlx->map.px[i].z / mlx->scale;
+//			mlx->map.px[i].y_w = mlx->map.px[i].y / mlx->scale;
+//			mlx->map.px[i].x_w = mlx->map.px[i].x / mlx->scale;
+////			mlx->map.px[i].z_w = mlx->map.px[i].y_w;
+//			i++;
+//		}
+//	}
 //	ft_write_double(mlx);
 }
