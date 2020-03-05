d3a2c7d386e293ad0aed4ccf6f059810a2fae265
cghael <a.p.gorobets@gmail.com>
2020-03-05 12:47:23 +0300

    scale does not work

--- includes/fdf.h
+++ includes/fdf.h
@@ -121,8 +121,8 @@ typedef struct		s_mlx
 	int				pressed;
 	double			angle_x;
 	double			angle_y;
-	int				scale_z;
-	int				scale;
+	double			scale_z;
+	double			scale;
 	int				slide;
 	int				win_x;
 	int				win_y;
