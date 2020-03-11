77be96347dc9e9c973882674510182140ddae11c
cghael <a.p.gorobets@gmail.com>
2020-03-11 18:46:02 +0300

    gradient 40/60

--- src/ft_draw_img_line.c
+++ src/ft_draw_img_line.c
@@ -27,15 +27,16 @@ static void		ft_gradient_step(t_point *start, t_point *end, t_mlx *mlx)
 	int r;
 	int g;
 	int b;
-	int step;
-
 
 	end_p.int_color = end->color;
 	start_p.int_color = start->color;
-	r = end_p.color.red - start_p.color.red;
-	g = end_p.color.green - start_p.color.green;
-	b = end_p.color.blue - start_p.color.blue;
-	step = ft_bigger_three(r, g, b);
+	r = ft_abs(end_p.color.red - start_p.color.red);
+	g = ft_abs(end_p.color.green - start_p.color.green);
+	b = ft_abs(end_p.color.blue - start_p.color.blue);
+	start->color_step.step = ft_bigger_three(r, g, b);
+	start->color_step.red_step = (double)r / (double)start->color_step.step;
+	start->color_step.green_step = (double)g / (double)start->color_step.step;
+	start->color_step.blue_step = (double)b / (double)start->color_step.step;
 }
 
 static void		ft_draw_img_pixel(t_mlx *mlx, t_point *point)
@@ -71,6 +72,38 @@ static void		ft_color_safty(t_point *start, t_point *end, t_mlx *mlx)
 	}
 }
 
+static char	ft_add_color_step(double max, t_point *start, t_point *end, int mv)
+{
+	return (((start->color >> mv & 0xff) - (end->color >> mv & 0xff)) / max);
+}
+
+static void		ft_color_shift(double max, t_point *start, t_point *end)
+{
+	double	tmp_r;
+	double	tmp_g;
+	double	tmp_b;
+	t_color_point	tmp;
+
+	tmp.int_color = start->color;
+	tmp.color.red += ft_add_color_step(max, start, end, 16);
+	tmp.color.green += ft_add_color_step(max, start, end, 8);
+	tmp.color.blue += ft_add_color_step(max, start, end, 0);
+//	point->buff.int_color = point->color;
+//	tmp_r = point->buff.color.red + point->color_step.red_step;
+//	tmp_g = point->buff.color.green + point->color_step.green_step;
+//	tmp_b = point->buff.color.blue + point->color_step.blue_step;
+//	point->buff.color.red = tmp_r;
+//	point->buff.color.green = tmp_g;
+//	point->buff.color.blue = tmp_b;
+//	point->color = point->buff.int_color;
+	start->color = tmp.int_color;
+}
+
+//static void		ft_color_delta(t_mlx *mlx, t_point *point)
+//{
+//	point->color_step.red_step +=
+//}
+
 void			ft_draw_img_line(t_point start, t_point end, t_mlx *mlx)
 {
 	double		x_step;
@@ -83,16 +116,20 @@ void			ft_draw_img_line(t_point start, t_point end, t_mlx *mlx)
 	x_step /= max;
 	y_step /= max;
 //	ft_color_safty(&start, &end, mlx);
-	ft_gradient_step(&start, &end, mlx);
+//	ft_gradient_step(&start, &end, max, mlx);
 	while ((int)(end.x_d - start.x_d) || (int)(end.y_d - start.y_d))
 	{
 		if ((int)start.x_d >= 0)
 		{
-//			ft_color_shift(mlx, &start);
+			if (start.color != end.color)
+				ft_color_shift(max, &start, &end);
 			ft_draw_img_pixel(mlx, &start);
 		}
 		start.x_d += x_step;
 		start.y_d += y_step;
+//		if (start.color != end.color)
+//			ft_color_shift(mlx, &start);
+//		ft_color_delta(mlx, &start);
 //		start.color += start.color_step;
 	}
 }
